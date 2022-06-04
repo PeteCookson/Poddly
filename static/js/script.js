05199d4d8c4c53a813eaefc2be5e4e2e7a8cd7c3
PeteCookson <pgcookson@gmail.com>
2022-06-04 15:24:42 +0000

    Add tooltip function to script.js and navbar icons

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-    $(".sidenav").sidenav({
-        edge: "right"
+    // alert auto show/hide
+    $(".alert").first().hide().slideDown(500).delay(1500).slideUp(500, function() {
+        $(this).remove();
+    });
+    // card flip
+    $('.card').click(function() {
+        $(this).toggleClass('flipped');
+    });
+    // tooltip
+    $(document).ready(function() {
+        $('[data-bs-toggle="tooltip"]').tooltip();
     });
 });
