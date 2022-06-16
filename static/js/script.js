acce3db378b43375c443be6179fccd416ee8052a
PeteCookson <pgcookson@gmail.com>
2022-06-16 02:58:52 +0000

    Meta tag added to head, removed redundant code from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,6 @@ $(document).ready(function() {
     $(".alert").first().hide().slideDown(500).delay(1500).slideUp(500, function() {
         $(this).remove();
     });
-    // card flip
-    $('.card').click(function() {
-        $(this).toggleClass('flipped');
-    });
     // tooltip
     $(document).ready(function() {
         $('[data-bs-toggle="tooltip"]').tooltip();
@@ -20,12 +16,4 @@ $(document).ready(function() {
             $(".pop-outer").fadeOut("slow");
         });
     });
-    // validate dropdown
-    /*    $("#category_name").validate({
-            messages: {
-                category_name: {
-                    required: "Please select an option from the list, if none are appropriate please select 'Other'",
-                },
-            }
-        });*/
 });
